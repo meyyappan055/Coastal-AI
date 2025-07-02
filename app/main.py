9403149bbbac028de8df7993778f26465fbd8c04
meyyappan055 <meyyappan055@gmail.com>
2025-07-02 16:42:28 +0530

    feat : indexing

--- app/main.py
+++ app/main.py
@@ -1,26 +1,40 @@
 import os
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.chat_models import init_chat_model
 from langchain_cohere import CohereEmbeddings
 from langchain_chroma import Chroma
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+pdf_path = BASE_DIR / "documents" / "reports" / "report1.pdf"
+
+if not pdf_path.exists():
+    raise FileNotFoundError(f"PDF not found at: {pdf_path}")
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-LANGSMITH_KEY = os.getenv("LANGSMITH_KEY")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+LANGSMITH_API_KEY = os.getenv("LANGSMITH_API_KEY")
+COHERE_KEY = os.getenv("COHERE_KEY")
 
 
-if not GEMINI_API_KEY or not LANGSMITH_KEY:
-    print("Please set the GEMINI_API_KEY and LANGSMITH_KEY environment variables.")
+if not GOOGLE_API_KEY or not LANGSMITH_API_KEY or not COHERE_KEY:
+    print("Please set the  environment variables properly.")
     exit(1)
 
 
 #LLM
-llm = init_chat_model("gemini-1.5-pro", model_provider="google_genai")
+llm = init_chat_model(
+    "gemini-1.5-pro",
+    model_provider="google_genai",
+    google_api_key=GOOGLE_API_KEY,
+)
 
 #Embeddings - Cohere
-embeddings = CohereEmbeddings(model="embed-english-v3.0")
+embeddings = CohereEmbeddings(model="embed-english-v3.0" , cohere_api_key=COHERE_KEY)
 
 
 #Vector Store - Chroma
@@ -29,4 +43,34 @@ vector_store = Chroma(
     embedding_function=embeddings,
     persist_directory="./chroma_langchain_db",
 )
+print("Vector store created.")
 
+
+#load the data
+
+loader = PyPDFLoader(str(pdf_path))
+documents = loader.load_and_split()
+
+async def load_pages_async(loader):
+    pages = []
+    async for page in loader.alazy_load():
+        pages.append(page)
+    return pages
+
+print("Loaded documents.")
+# print(f"{documents[0].metadata}\n")
+# print(documents[1].page_content)
+
+
+#Chunking (Later -> recursive splitter + semantic splitter)
+
+recursive_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,       
+    chunk_overlap=200,   
+    separators=["\n\n", "\n", ".", " ", ""]
+)
+
+documents = recursive_splitter.split_documents(documents)
+
+# print(f"Number of chunks: {len(documents)}")
+# print(f"Chunk content: {(documents[0].page_content)}")
